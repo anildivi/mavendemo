62d89e4ad26d4e4bd590f19af1a513e6add6db9e
Anitha Ruth <anildivi@gmail.com>
2025-12-30 18:53:06 +0530

    redo

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -5,7 +5,6 @@ package com.example.demo;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!")
-        System.out.println("hi Roopa!");
+        System.out.println("Hello World!");
     }
 }
