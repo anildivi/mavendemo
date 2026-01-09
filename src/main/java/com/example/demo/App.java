ae4892cfe774aa49f0e3054c7cead6395bce45da
Anitha Ruth <anildivi@gmail.com>
2026-01-09 13:29:34 +0530

    branch test save

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -6,6 +6,6 @@ package com.example.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!")
-        System.out.println("hi all!");
+        System.out.println("demotest!");
     }
 }
